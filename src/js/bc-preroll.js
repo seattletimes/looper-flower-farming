3cae09945c2b57da1c7c8d81152c19b7646c43bc
emilymeng <eeng@seattletimes.com>
2021-01-29 11:41:13 -0800

    Update bc-preroll.js

--- src/js/bc-preroll.js
+++ src/js/bc-preroll.js
@@ -2,7 +2,7 @@ var $ = require("./lib/qsa");
 
 var makePlayer = function(id) {
   return `
-  <iframe src="https://cdn.jwplayer.com/players/${id}-UXknQA8J.html" width="100%" height="100%" frameborder="0" scrolling="auto" title="Pike Place&#x27;s Hmong flower farmers call on generations of resilience" style="position:absolute;" allowfullscreen></iframe>
+  <iframe src="https://cdn.jwplayer.com/players/${id}-oNGue23q.html" width="100%" height="100%" frameborder="0" scrolling="auto" title="Pike Place&#x27;s Hmong flower farmers call on generations of resilience" style="position:absolute;" allowfullscreen></iframe>
 `;
 };
 
